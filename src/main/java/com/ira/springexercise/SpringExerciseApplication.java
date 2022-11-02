562710967b8b1dd8d69b1b8d128a0084290e2288
Ira Ungos <ira_clark_ungos@dlsu.edu.ph>
2022-11-02 14:21:13 +0800

    Trying Heroku.

--- src/main/java/com/ira/springexercise/SpringExerciseApplication.java
+++ src/main/java/com/ira/springexercise/SpringExerciseApplication.java
@@ -4,12 +4,10 @@ import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@Configuration
 public class SpringExerciseApplication {
 
     public static void main(String[] args) {
@@ -21,7 +19,7 @@ public class SpringExerciseApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://ira-renzo.github.io/angular-exercise");
+                registry.addMapping("/**").allowedOrigins("https://spring-backend-ira.herokuapp.com/");
             }
         };
     }
