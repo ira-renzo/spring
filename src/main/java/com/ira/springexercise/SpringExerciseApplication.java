afabd989c4a8152b9e48dfa4cb482f1d7fc1d2c0
Ira Ungos <ira_clark_ungos@dlsu.edu.ph>
2022-11-08 03:37:48 +0800

    Modified CORS Configuration.

--- src/main/java/com/ira/springexercise/SpringExerciseApplication.java
+++ src/main/java/com/ira/springexercise/SpringExerciseApplication.java
@@ -19,9 +19,7 @@ public class SpringExerciseApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://ira-renzo.github.io", "http://localhost:4200")
-                        .allowedMethods("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
             }
         };
     }
